83f26e5f6d5f92620f1e1584c98937f0dedc3239
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2023-08-09 20:10:57 -0400

    Fixed WhichKey timeout

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "carbonfox",
+  colorscheme = "gruvbox-baby",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -17,7 +17,6 @@ return {
     {
         "folke/which-key.nvim",
         init = function ()
-            vim.o.timeout = true
             vim.o.timeoutlen = 1000
         end,
         opts = {},
