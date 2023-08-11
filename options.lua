e096adf8ced243e702a99127529f4c9eca08af79
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2023-08-11 14:30:38 -0400

    Attempt to Fix Indenting

--- options.lua
+++ options.lua
@@ -8,6 +8,11 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = true, -- sets vim.opt.wrap
     guicursor = "i:block",
+    shiftwidth = 4,
+    tabstop = 4,
+    expandtab = false,
+    autoindent = true,
+    -- cindent = true,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
@@ -18,9 +23,6 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
-    shiftwidth = 4,
-    tabstop = 4,
-    expandtab=true 
   },
 }
 -- If you need more control, you can use the function()...end notation
